11b65366b2fef03f63c30964a8d5c4f639f79bc7
DiegoG1117 <130846543+DiegoG1117@users.noreply.github.com>
2023-05-24 13:41:47 -0500

    contador de muerte finalizado

--- Assets/Scripts/ContadorMuerte.cs
+++ Assets/Scripts/ContadorMuerte.cs
@@ -1,25 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class ContadorMuerte : MonoBehaviour
 {
-  public Canvas canvasToEnable; 
+    public Canvas canvasToEnable;
+    public TextMeshProUGUI contadorText;
+    public float tiempoInicial = 10f;
 
-    // Start is called before the first frame update
-    void Start()
+    private float tiempoRestante;
+
+    private void Start()
     {
-        
+        canvasToEnable.enabled = false;
+        contadorText.text = tiempoInicial.ToString();
+        tiempoRestante = tiempoInicial;
     }
-      private void OnTriggerEnter(Collider other)
+
+    private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-
             canvasToEnable.enabled = true;
+            InvokeRepeating("ActualizarContador", 1f, 1f);
         }
-
-
     }
-    
+
+    private void ActualizarContador()
+    {
+        tiempoRestante -= 1f;
+        contadorText.text = tiempoRestante.ToString();
+
+        if (tiempoRestante <= 0f)
+        {
+            CancelInvoke("ActualizarContador");
+            SceneManager.LoadScene(0); // Carga la escena número 0
+        }
+    }
 }
