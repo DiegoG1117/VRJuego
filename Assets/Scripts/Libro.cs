c04aca72f20cf6b57c848e27cf3fb4513e3e9ce3
AyrtonAvilar <130846785+AyrtonAvilar@users.noreply.github.com>
2023-05-28 12:51:01 -0500

    Libro avance
    Se modifico las carpetas dodne estaban las texturas y se agrego, la animacion de la colicion y gravedad en los libros de manera exitosa porfin, se añadio 2 obeliscos donde estaran los letretos de intrucciones o en dicho caso acertijos para realizar los retos

--- Assets/Scripts/Libro.cs
+++ Assets/Scripts/Libro.cs
@@ -1,45 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-using UnityEngine;
+//using UnityEngine;
 
-public class Libro : MonoBehaviour
-{
-    public bool esCorrecto = false;
-    public MesaLibro mesaLibro;
-    public string nombreLibro = Imaginacion;
+//public class Libro : MonoBehaviour
+//{
+//    public bool esCorrecto = false;
+//    public MesaLibro mesaLibro;
+//    public string nombreLibro = Imaginacion;
 
     // Método llamado cuando el jugador interactúa con el libro
-    public void Agarrar()
-    {
+    //public void Agarrar()
+    //{
         // Desactivar la física del libro para que pueda ser manipulado
-        GetComponent<Rigidbody>().isKinematic = true;
+        //GetComponent<Rigidbody>().isKinematic = true;
 
         // Desactivar el collider para evitar interacciones no deseadas
-        GetComponent<Collider>().enabled = false;
+        //GetComponent<Collider>().enabled = false;
 
         // Mostrar mensaje con el nombre del libro
-        Debug.Log("Has agarrado el libro: " + nombreLibro);
-    }
+        //Debug.Log("Has agarrado el libro: " + nombreLibro);
+    //}
 
     // Método llamado cuando el jugador suelta el libro
-    public void Soltar()
-    {
+    //public void Soltar()
+    //{
         // Activar la física del libro para que vuelva a tener interacción con el entorno
-        GetComponent<Rigidbody>().isKinematic = false;
+        //GetComponent<Rigidbody>().isKinematic = false;
 
         // Activar el collider para que vuelva a interactuar con otros objetos
-        GetComponent<Collider>().enabled = true;
+        //GetComponent<Collider>().enabled = true;
 
         // Verificar si el libro es el correcto y notificar a la mesa
-        if (esCorrecto)
-        {
-            mesaLibro.LibroCorrectoColocado(true);
-        }
-        else
-        {
-            mesaLibro.LibroIncorrectoColocado();
-        }
-    }
-}
+        //if (esCorrecto)
+        //{
+           // mesaLibro.LibroCorrectoColocado(true);
+       //}
+        //else
+        //{
+           // mesaLibro.LibroIncorrectoColocado();
+        //}
+    //}
+//}
